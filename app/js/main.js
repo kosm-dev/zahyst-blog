693cd712d4490064f06624e0779eb0a6395e3d3b
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2024-02-12 00:25:56 +0200

    added header and navbar

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,61 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+
+  function toggleMenu() {
+    const burger = document.querySelector('.burger'),
+      menu = document.querySelector('.header__menu'),
+      body = document.querySelector('body');
+
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('burger--active');
+      menu.classList.toggle('active');
+      body.classList.toggle('lock');
+    });
+  }
+
+  if(document.querySelector('.burger')) {
+    toggleMenu();
+  }
+
+
+
+  function headerScroll(headerClass) {
+    const header = document.querySelector(headerClass);
+
+    function onScroll() {
+      if (window.scrollY > 0) {
+        header.classList.add('header--scroll');
+      } else {
+        header.classList.remove('header--scroll');
+      }
+    }
+    window.addEventListener('scroll', onScroll);
+  }
+
+  if (document.querySelector('.header')) {
+    headerScroll('.header');
+  }
+
+
+  // function hideNavbar(navbarClass) {
+  //   const navbar = document.querySelector(navbarClass);
+
+  //   function onScroll() {
+  //     if (window.scrollY > 0) {
+  //       navbar.classList.add('active');
+  //     } else {
+  //       navbar.classList.remove('active');
+  //     }
+  //   }
+
+  //   window.addEventListener('scroll', onScroll);
+  // }
+
+  // if (document.querySelector('.navbar')) {
+  //   hideNavbar('.navbar');
+  // }
+
+
+
 });
